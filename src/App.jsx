4a6ce5c410fa2ba8c42d13041ee571e8ffb1885d
unknown <artemix926@gmail.com>
2022-10-15 00:03:11 +0300

    src commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./App.css";
+
+import {
+  Footer,
+  WhatGPT3,
+  Features,
+  Header,
+  Blog,
+  Possibility,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
